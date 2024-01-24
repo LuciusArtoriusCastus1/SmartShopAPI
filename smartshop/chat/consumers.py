21e218f973492c19a9126aada6a0e584592db138
ErgoProxy <kolaporajko2356@gmail.com>
2024-01-24 17:56:30 +0200

    Alors on Danse

--- smartshop/chat/consumers.py
+++ smartshop/chat/consumers.py
@@ -47,7 +47,7 @@ class ChatRoomConsumer(ObserverModelInstanceMixin, GenericAsyncAPIConsumer):
 
     @message_activity.groups_for_signal
     def message_activity(self, instance: Message, **kwargs):
-        yield f'room__{instance.chat_room_id}'
+        yield f'room__{instance.chat_room.id}'
         yield f'pk__{instance.pk}'
 
     @message_activity.groups_for_consumer
